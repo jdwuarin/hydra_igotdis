3f5d8a494d6e04b985d068ebe4185886c70c324b
john-dwuarin <john.dwuarin@gmail.com>
2014-09-27 06:20:28 +0100

    and again

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Hydra
     I18n.config.enforce_available_locales = true
     config.autoload_paths += %W["#{config.root}/app/validators/"]
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif, *.js, *.css)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
   end
 end
