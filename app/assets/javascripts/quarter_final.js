bf40297b7ba6dc5d9e69f7fc3c69a6ebde76cd8e
john-dwuarin <john.dwuarin@gmail.com>
2014-10-02 02:05:32 +0100

    team buttons now act as expected and can submit

--- app/assets/javascripts/quarter_final.js
+++ app/assets/javascripts/quarter_final.js
@@ -4,13 +4,17 @@ $('th.Logo').on('click', function (event) {
   $(this).siblings('th').removeClass('active');
   $(this).addClass('active');
 
-  // update the form
+  // update the forms (predicted contestant depends only on this click)
   var predicted_contestant = $(this).children(
     "div.contestant")[0]["textContent"];
 
-  var input_to_change = $(this).parents("div.matches-line").
-    find("form.winner-form").children("#predicted_contestant_id")[0];
-  input_to_change["value"] = predicted_contestant;
+  $(this).parents("div.matches-line").find(
+    "form").children("#predicted_contestant_id").each(function() {
+
+      $(this)[0]["value"] = predicted_contestant;
+
+  });
+
 
   // deal with making plus minus buttons appear
   // and the value of the score
@@ -18,10 +22,23 @@ $('th.Logo').on('click', function (event) {
   var table_row = $(this).parents("div.matches-line").find(
     "tbody.scorelol").children("tr");
 
-  console.tr(table_row);
+  table_row.children().each(function () {
 
-  table_row.children("th#" + predicted_contestant).
-    children("h1")["text"] = 2;
+    if ($(this)[0]["id"] == predicted_contestant) {
+      $(this).find(".minuslogo").hide();
+      $(this).find(".pluslogo").hide();
+      $(this).find("h1")[0]["textContent"] = "2";
+    }
+    else {
+      $(this).find(".minuslogo").show();
+      $(this).find(".pluslogo").show();
+      $(this).find("h1")[0]["textContent"] = "0";
+      // because if user pressed up it would be type 6
+      $(this).parents("div.matches-line").find(
+        "form.score-form").children("#prediction_type")[0]["value"] = 5;
+    }
+
+  });
 
   // // enable button
   // button = $(this).parents("table").
@@ -32,6 +49,43 @@ $('th.Logo').on('click', function (event) {
 
 });
 
+$("a.btn-submit").on("click", function(e) {
+
+  $(this).parents("div.col-md-12").find(
+    "form.winner-form").trigger("submit");
+  $(this).parents("div.col-md-12").find(
+    "form.score-form").trigger("submit");
+
+});
+
+$("img.minuslogo").on("click", function(e) {
+
+  var minus_sign = $(this).parent().siblings("div#score").find("h1");
+
+  if (minus_sign[0]["textContent"] == "1"){
+    minus_sign[0]["textContent"] = "0";
+
+  $(this).parents("div.matches-line").find(
+    "form.score-form").children("#prediction_type")[0]["value"] = 5;
+
+  }
+
+});
+
+
+$("img.pluslogo").on("click", function(e) {
+
+  var plus_sign = $(this).parent().siblings("div#score").find("h1");
+
+  if (plus_sign[0]["textContent"] == "0"){
+    plus_sign[0]["textContent"] = "1";
+
+  $(this).parents("div.matches-line").find(
+    "form.score-form").children("#prediction_type")[0]["value"] = 6;
+
+  }
+
+});
 
 $("form.winner-form").on('ajax:success', function(data, status, xhr) {
 
@@ -53,14 +107,5 @@ $("form.winner-form").on('ajax:error', function(xhr, status, error) {
 
     button[0]["text"] = "There was an error, please try again";
     button.removeClass("btn-default");
-
-});
-
-$("a.btn-submit").on("click", function(e) {
-
-  $(this).parents("div.col-md-12").find(
-    "form.winner-form").trigger("submit");
-  $(this).parents("div.col-md-12").find(
-    "form.score-form").trigger("submit");
 
 });
