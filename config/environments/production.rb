2b63f4a858cff78ea22d7c98af40d8ccb8e0956c
john-dwuarin <john.dwuarin@gmail.com>
2014-10-17 16:02:56 +0100

    fixed small confirmation bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Hydra::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.smtp_settings = {
+    from: "iGotDis",
     address: 'smtp.gmail.com',
     port: '587',
     enable_starttls_auto: true,
