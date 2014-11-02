c99e19d5afab151e1c177fcf6afe9429a783044b
john-dwuarin <john.dwuarin@gmail.com>
2014-11-02 00:23:48 +0000

    can now login (data is not persisted on the client side yet tho...)

--- app/assets/javascripts/controllers/authentication_modal.js
+++ app/assets/javascripts/controllers/authentication_modal.js
@@ -7,24 +7,47 @@ App.AuthenticationModalController = Ember.Controller.extend({
   actions: {
     goToLogin: function() {
       this.set('title', 'Login');
+      this.clearModal();
       this.set('isLoginModal', true);
     },
     goToSignup: function() {
       this.set('title', 'Signup');
+      this.clearModal();
       this.set('isLoginModal', false);
     },
     login: function() {
-      this.set('login', '');
-      this.set('password', '');
-      this.set('isPresent', false);
+
+      var data = {
+        user: {
+          login: this.get('login'),
+          password: this.get('password')
+        }
+      };
+
+      Ember.$.post('users/sign_in.json', data).then(function(response) {
+        alert('got a response');
+      });
+
+      this.clearModal();
+      this.closeModal();
+
     },
     signup: function() {
       // this needs to be done upon success only.
-      this.set('username', '');
-      this.set('email', '');
-      this.set('password', '');
-      this.set('passwordConfirmation', '');
-      this.set('isPresent', false);
+      this.clearModal();
+      this.closeModal();
     }
+  },
+
+  clearModal: function() {
+    this.set('login', '');
+    this.set('username', '');
+    this.set('email', '');
+    this.set('password', '');
+    this.set('passwordConfirmation', '');
+  },
+
+  closeModal: function() {
+    this.set('isPresent', false);
   }
 });
--- app/assets/javascripts/routes/application.js
+++ app/assets/javascripts/routes/application.js
@@ -26,20 +26,12 @@ App.ApplicationRoute = Ember.Route.extend({
 
   showSignupModal: function() {
     name = 'authentication-modal';
-    model = Ember.Object.create({username: '',
-                                 email: '',
-                                 password: '',
-                                 passwordConfirmation: '' });
-    this.controllerFor(name).set('content', model);
     this.controllerFor(name).set('isLoginModal', false);
     this.controllerFor(name).set('isPresent', true);
     this.controllerFor(name).set('title', 'Signup');
   },
   showLoginModal: function() {
     name = 'authentication-modal';
-    model = Ember.Object.create({login: '',
-                                 password: ''});
-    this.controllerFor(name).set('content', model);
     this.controllerFor(name).set('isLoginModal', true);
     this.controllerFor(name).set('isPresent', true);
     this.controllerFor(name).set('title', 'Login');
